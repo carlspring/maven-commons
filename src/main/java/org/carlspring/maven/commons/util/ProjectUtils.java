540b7a1885dd9de8cf5fb7c4fcf534766e1b6bc6
Martin Todorov <carlspring@gmail.com>
2013-07-05 18:37:24 +0100

    Added a method.

--- src/main/java/org/carlspring/maven/commons/util/ProjectUtils.java
+++ src/main/java/org/carlspring/maven/commons/util/ProjectUtils.java
@@ -1,6 +1,8 @@
 package org.carlspring.maven.commons.util;
 
-import org.apache.maven.project.MavenProject;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.project.*;
 
 /**
  * @author mtodorov
@@ -12,6 +14,19 @@ public class ProjectUtils
     {
         return project.getArtifact().getGroupId().equals("org.apache.maven") &&
                project.getArtifact().getArtifactId().equals("standalone-pom");
+    }
+
+    public static MavenProject getProjectForArtifact(Artifact artifact,
+                                                     MavenSession session,
+                                                     ProjectBuilder projectBuilder)
+            throws ProjectBuildingException
+    {
+        final DefaultProjectBuildingRequest request = new DefaultProjectBuildingRequest();
+        request.setRepositorySession(session.getRepositorySession());
+
+        final ProjectBuildingResult result = projectBuilder.build(artifact, request);
+
+        return result.getProject();
     }
 
 }

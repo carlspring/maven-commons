9a15a3fc792fbf23554d141e519788ebc61d1937
carlspring <carlspring@gmail.com>
2016-03-21 21:22:41 +0000

    SB-459: Fix the failing tests under Windows
    Fixed the path separator issue in the the maven-commons module.

--- src/main/java/org/carlspring/maven/commons/util/ArtifactUtils.java
+++ src/main/java/org/carlspring/maven/commons/util/ArtifactUtils.java
@@ -66,7 +66,7 @@ public class ArtifactUtils
         StringBuilder classifier = new StringBuilder();
         String type = null;
 
-        String[] pathElements = path.split("/");
+        String[] pathElements = path.split("/|\\\\");
 
         if (pathIsCompleteGAV(path))
         {
